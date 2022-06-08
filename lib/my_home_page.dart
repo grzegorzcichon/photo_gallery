5ae958933ffcd7fc50bc969b3cb44327e188391b
Grzegorz Cichioń <grzegorz@cichon.dev>
2022-06-08 11:43:31 +0200

    Add BottomNavigationBar

---
+++ lib/my_home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:photo_gallery/photo.dart';
+import 'package:photo_gallery/photo_list.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var currentindex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder<List<Photo>>(
+        future: fetchPhotos(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('An error has occurred!'),
+            );
+          } else if (snapshot.hasData) {
+            return PhotosList(photos: snapshot.data!);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentindex,
+          onTap: (newIndex) {
+            setState(() {
+              currentindex = newIndex;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.keyboard_double_arrow_left_rounded),
+                label: 'Photo Gallery'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.keyboard_double_arrow_right_outlined),
+                label: 'Comments list'),
+          ]),
+    );
+  }
+}
---
+++ lib/photo_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:photo_gallery/photo.dart';
+
+class PhotosList extends StatelessWidget {
+  const PhotosList({super.key, required this.photos});
+
+  final List<Photo> photos;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 4,
+        mainAxisSpacing: 4,
+      ),
+      itemCount: photos.length,
+      itemBuilder: (context, index) {
+        return Image.network(photos[index].thumbnailUrl);
+      },
+    );
+  }
+}
